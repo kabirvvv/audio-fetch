7e3a783f0ec9e1576f3191dcef7b66ad9aecda2f
kabirvvv <kabirv338@gmail.com>
2026-06-23 12:51:49 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,10 @@ android {
         targetSdk = 34
         versionCode = 10
         versionName = "10"
+    buildFeatures { viewBinding = true; buildConfig = true }
+
         ndk {
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("arm64-v8a")
         }
     }
     // --- NEW: signing config for release builds ---
@@ -63,4 +65,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-session:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
+    implementation("androidx.palette:palette-ktx:1.0.0")
 }
