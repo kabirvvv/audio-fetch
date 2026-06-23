b058baefaab10ca206736b925d86f2d62956e660
kabirvvv <kabirv338@gmail.com>
2026-06-23 13:30:06 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.audiofetch"
+
         minSdk = 24
         targetSdk = 34
 
@@ -24,6 +25,7 @@ android {
     signingConfigs {
         create("release") {
             storeFile = file("release.keystore")
+
             storePassword = System.getenv("RELEASE_STORE_PASSWORD")
             keyAlias = System.getenv("RELEASE_KEY_ALIAS")
             keyPassword = System.getenv("RELEASE_KEY_PASSWORD")
@@ -70,15 +72,23 @@ chaquopy {
 }
 
 dependencies {
+
+    // AndroidX Core
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    // Material Design
     implementation("com.google.android.material:material:1.12.0")
 
+    // Layouts
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 
+    // Media3
     implementation("androidx.media3:media3-exoplayer:1.3.1")
     implementation("androidx.media3:media3-session:1.3.1")
     implementation("androidx.media3:media3-ui:1.3.1")
 
+    // Dynamic Colors
     implementation("androidx.palette:palette-ktx:1.0.0")
 }
