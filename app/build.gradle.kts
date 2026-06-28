704b5a4ed34bf95a21ced34ba5e3113a296faef6
kabirvvv <kabirv338@gmail.com>
2026-06-28 11:42:47 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 24
         targetSdk = 34
 
-        versionCode = 35
-        versionName = "3.5"
+        versionCode = 36
+        versionName = "4.0"
 
         ndk {
             abiFilters += "arm64-v8a"
