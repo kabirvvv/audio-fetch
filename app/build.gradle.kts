a1d5b78d3b62a55e2388efb4d4748d332348b19e
kabirvvv <kabirv338@gmail.com>
2026-09-04 11:03:54 +0530

    Update version code and name to 71 and '5.1'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 24
         targetSdk = 34
 
-        versionCode = 70
-        versionName = "5.0"
+        versionCode = 71
+        versionName = "5.1"
 
         ndk {
             abiFilters += "arm64-v8a"
